202a991122ea4c695e2f0d36ac521665fa1fb8d6
Nnnn <kalybekovasaikal@gmail.com>
2026-05-03 23:10:11 +0600

    added checking phone

--- server/src/routes/authRoutes.js
+++ server/src/routes/authRoutes.js
@@ -1,12 +1,10 @@
 import express from 'express';
-import {
-  startRegister,
-  login,
-  forgotPassword
-} from '../controllers/authController.js';
+import { startRegister, login, forgotPassword, checkPhone } from '../controllers/authController.js';
+
 
 const router = express.Router();
 
+router.post('/check-phone', checkPhone);
 router.post('/start-register', startRegister);
 router.post('/login', login);
 router.post('/forgot-password', forgotPassword);
